1cc84d1584e2d8099b9dc0e09cee0a8486db5e99
Svetlin Ralchev <iamralch@users.noreply.github.com>
2026-03-11 15:29:38 +0400

    fix(nvim-rust): add --branch flag and fix lcov output path to target/lcov.info

--- lua/nvim-plugins/nvim-rust/init.lua
+++ lua/nvim-plugins/nvim-rust/init.lua
@@ -7,7 +7,7 @@ return {
 			return {}
 		end
 
-		local has_coverage, coverage_config = pcall(require, "coverage.config")
+		local has_coverage, _ = pcall(require, "coverage.config")
 		if not has_coverage then
 			return opts
 		end
@@ -20,14 +20,12 @@ return {
 					if spec then
 						local cmd = spec.command
 						if cmd[1] == "cargo" then
-							local coverage_file = vim.tbl_get(coverage_config.opts, "lang", "rust", "coverage_file")
-									or "lcov.info"
-							local lcov_path = vim.fn.getcwd() .. "/" .. coverage_file
-
+							local lcov_path = vim.fn.getcwd() .. "/target/lcov.info"
 							table.insert(cmd, 2, "llvm-cov")
 							table.insert(cmd, 3, "--lcov")
-							table.insert(cmd, 4, "--output-path")
-							table.insert(cmd, 5, lcov_path)
+							table.insert(cmd, 4, "--branch")
+							table.insert(cmd, 5, "--output-path")
+							table.insert(cmd, 6, lcov_path)
 						end
 					end
 					return spec
